414823401b04a84cb781009e4890abc0e421b722
derya <lacin>
2024-10-21 11:25:57 +0200

    message added

--- lettre.js
+++ lettre.js
@@ -5,14 +5,12 @@ let randomMot = mots[randomIndex];
 let tabloLettre = lettres.split("");
 console.log(randomMot);
 console.log(tabloLettre);
-let divLettres=document.createElement("div")
-divLettres.className="headline";
+let divLettres = document.createElement("div");
+divLettres.className = "headline";
 
-let divHeader= document.getElementsByClassName("headline")
-let buttonContainer = document.createElement("div")
-buttonContainer.className="button"
-
-
+let divHeader = document.getElementsByClassName("headline");
+let buttonContainer = document.createElement("div");
+buttonContainer.className = "button";
 
 let divContainer = document.createElement("div");
 divContainer.className = "container";
@@ -22,12 +20,18 @@ let h1 = document.createElement("h1");
 h1.innerText = randomMot;
 divLettres.appendChild(h1);
 let h2 = document.createElement("h2");
-h2.innerText=" "
-container[0].appendChild(divLettres)
+h2.innerText = " ";
+container[0].appendChild(divLettres);
 divLettres.appendChild(h2);
-container[0].appendChild(buttonContainer)
+container[0].appendChild(buttonContainer);
+let message = document.createElement("p");
+divContainer.appendChild(message);
+
+let h3 = document.createElement("h3");
+divLettres.appendChild(h3);
+
 let placeholder = [];
-let currentIndex = 0; 
+
 for (let i = 0; i < randomMot.length; i++) {
   placeholder.push("_");
 }
@@ -42,15 +46,15 @@ for (let i = 0; i < tabloLettre.length; i++) {
   let button = document.getElementsByClassName("btn");
   button[i].addEventListener("click", function () {
     let lettres = button[i].innerText;
-    if (randomMot[currentIndex] === lettres) {  // Check if the letter matches the current index
-        placeholder[currentIndex] = lettres;  // Update the placeholder
-        currentIndex++;  // Move to the next index
-    }
-
 
     for (let j = 0; j < randomMot.length; j++) {
       if (randomMot[j] === lettres) {
         placeholder[j] = lettres;
+      }
+      if (randomMot.includes(lettres)) {
+        message.innerText = "bravo";
+      } else {
+        message.innerText = "wrong letter";
       }
     }
 
@@ -58,6 +62,3 @@ for (let i = 0; i < tabloLettre.length; i++) {
     console.log(placeholder);
   });
 }
-
-
-
